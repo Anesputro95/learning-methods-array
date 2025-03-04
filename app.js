450ef324cff065980b39c9deb258d2a07140735c
AnesPutro <anes.putro@gmail.com>
2025-03-04 14:37:25 +0700

    Methods Array

---
+++ app.js
@@ -0,0 +1,15 @@
+// Method Push
+let sleepTogether = ['John', 'Nana']
+sleepTogether.push('Mia')
+console.log(sleepTogether)
+
+// Method Pop
+let playGames = ['Jinc', 'Virgi', 'Abi', 'Omi']
+playGames.pop()
+console.log(playGames)
+
+// Method Shift and unshift
+let letsGoo = ["Bogor", "Bandung", "Jogja", "Papua", "Bali"]
+letsGoo.shift();
+letsGoo.unshift('Lembang');
+console.log(letsGoo)
